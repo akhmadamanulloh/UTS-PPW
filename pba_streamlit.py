c3f78c914f9d5a76ac8eea612ba5c89f5f8b25ca
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2023-10-19 07:54:47 +0700

    Update pba_streamlit.py

--- pba_streamlit.py
+++ pba_streamlit.py
@@ -10,7 +10,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans  
 from sklearn import tree
 
-Data, Ekstraksi, lda, LDAkmeans, Model = st.tabs(['Data', 'Ekstraksi Fitur', 'LDA', 'LDA kmeans', 'Modelling'])
+Data, Ekstraksi, lda, LDAkmeans, Model ,implementasi = st.tabs(['Data', 'Ekstraksi Fitur', 'LDA', 'LDA kmeans', 'Modelling', 'implementasi'])
 
 with Data :
    st.title("""UTS PPW A""")
@@ -72,33 +72,78 @@ with Model :
 
         st.write ("Pilih metode yang ingin anda gunakan :")
         met1 = st.checkbox("KNN")
-        # if met1 :
-        #     st.write("Hasil Akurasi Data Training Menggunakan KNN sebesar : ", (100 * metode1.score(X_train, y_train)))
-        #     st.write("Hasil Akurasi Data Testing Menggunakan KNN sebesar : ", (100 * (metode1.score(X_test, y_test))))
         met2 = st.checkbox("Naive Bayes")
-        # if met2 :
-        #     st.write("Hasil Akurasi Data Training Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_train, y_train)))
-        #     st.write("Hasil Akurasi Data Testing Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_test, y_test)))
         met3 = st.checkbox("Decesion Tree")
-        # if met3 :
-            # st.write("Hasil Akurasi Data Training Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_train, y_train)))
-            # st.write("Hasil Akurasi Data Testing Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_test, y_test)))
         submit2 = st.button("Pilih")
 
         if submit2:      
             if met1 :
-                st.write("Metode yang Anda gunakan Adalah KNN")
-                st.write("Hasil Akurasi Data Training Menggunakan KNN sebesar : ", (100 * metode1.score(X_train, y_train)))
-                st.write("Hasil Akurasi Data Testing Menggunakan KNN sebesar : ", (100 * (metode1.score(X_test, y_test))))
+                st.write("KNN")
+                st.write("Hasil Akurasi Menggunakan KNN sebesar : ", (100 * metode1.score(X_train, y_train)))
             elif met2 :
-                st.write("Metode yang Anda gunakan Adalah Naive Bayes")
-                st.write("Hasil Akurasi Data Training Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_train, y_train)))
-                st.write("Hasil Akurasi Data Testing Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_test, y_test)))
+                st.write("Naive Bayes")
+                st.write("Hasil Akurasi Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_train, y_train)))
             elif met3 :
-                st.write("Metode yang Anda gunakan Adalah Decesion Tree")
-                st.write("Hasil Akurasi Data Training Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_train, y_train)))
-                st.write("Hasil Akurasi Data Testing Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_test, y_test)))
+                st.write("Decesion Tree")
+                st.write("Hasil Akurasi Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_train, y_train)))
             else :
                 st.write("Anda Belum Memilih Metode")
-    # else:
-    #     st.write("Anda Belum Menentukan Jumlah Topik di Menu LDA")
+with implementasi:
+        import pandas as pd
+        import numpy as np
+        from sklearn.decomposition import LatentDirichletAllocation
+        from sklearn.naive_bayes import MultinomialNB
+        from sklearn.feature_extraction.text import TfidfVectorizer
+        from sklearn.model_selection import train_test_split
+        from sklearn.metrics import accuracy_score
+
+        # Baca data
+        data_x = pd.read_csv('https://raw.githubusercontent.com/akhmadamanulloh/pba/main/Term%20Frequensi%20Berlabel%20Final.csv')
+        data_x = data_x.dropna(subset=['Dokumen'])  # Menghapus baris yang memiliki NaN di kolom 'Dokumen'
+
+        # Ubah kelas A menjadi 0 dan kelas B menjadi 1
+        kelas_dataset_binary = [0 if kelas == 'RPL' else 1 for kelas in data_x['Label']]
+        data_x['Label'] = kelas_dataset_binary
+
+        # Bagi data menjadi data pelatihan dan data pengujian
+        X = data_x['Dokumen']
+        label = data_x['Label']
+        X_train, X_test, y_train, y_test = train_test_split(X, label, test_size=0.2, random_state=42)
+
+        # Vektorisasi teks menggunakan TF-IDF
+        tfidf_vectorizer = TfidfVectorizer()
+        X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+        X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+        # Latih model Naive Bayes
+        nb_classifier = MultinomialNB()
+        nb_classifier.fit(X_train_tfidf, y_train)
+
+        # Latih model LDA
+        k = 3
+        alpha = 0.1
+        beta = 0.2
+        lda = LatentDirichletAllocation(n_components=k, doc_topic_prior=alpha, topic_word_prior=beta)
+        proporsi_topik_dokumen = lda.fit_transform(X_train_tfidf)
+
+        
+
+        with st.form("my_form"):
+            st.subheader("Implementasi")
+            input_dokumen = st.text_input('Masukkan Judul Yang Akan Diklasfifikasi')
+            input_vector = tfidf_vectorizer.transform([input_dokumen])
+            submit = st.form_submit_button("submit")
+            # Prediksi proporsi topik menggunakan model LDA
+            proporsi_topik = lda.transform(input_vector)[0]
+            if submit:
+                st.subheader('Hasil Prediksi')
+                inputs = np.array([input_dokumen])
+                input_norm = np.array(inputs)
+                input_pred = nb_classifier.predict(input_vector)[0]
+            # Menampilkan hasil prediksi
+                if input_pred==0:
+                    st.success('RPL')
+                    st.write(proporsi_topik)
+                else  :
+                    st.success('KK')
+                    st.write(proporsi_topik)
